11a4fe9da3e3b084cf5199aadc531c13bf1e65ce
Denis <ignatenko.de2017@yandex.ru>
2024-05-05 14:06:38 +0300

    add isAdmin endpoind

---
+++ Api/Controllers/AdminsController.cs
@@ -0,0 +1,13 @@
+﻿using Api.Common;
+using Api.Common.Filter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("api/admins")]
+public class AdminsController : ApiController
+{
+    [CustomAuthorize]
+    [HttpGet("isAdmin")]
+    public bool IsAdmin() => true;
+}
